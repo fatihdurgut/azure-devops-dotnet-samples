2b43ec5583436fb1df93ed6bfcaeb78ccbd621e6
Fatih <fatih@durgut.com>
2024-01-23 14:21:43 +0300

    test commit

--- ClientLibrary/Quickstarts/netcore/ShowWorkItemConsole/Program.cs
+++ ClientLibrary/Quickstarts/netcore/ShowWorkItemConsole/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.TeamFoundation.WorkItemTracking.WebApi;
 using Microsoft.TeamFoundation.WorkItemTracking.WebApi.Models;
 using Microsoft.VisualStudio.Services.Common;
 using Microsoft.VisualStudio.Services.WebApi;
+//test commit 
 
 namespace ConsoleApp
 {
